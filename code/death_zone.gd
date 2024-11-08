f5d3a9b9a2b81cf18f09934110b5976b8d828703
The YaviK Team <theyavikteam@gmail.com>
2024-11-08 14:49:53 +0100

    Improve player death

--- code/death_zone.gd
+++ code/death_zone.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
+const SLOW_TIME_SCALE = 0.3
+const NORMAL_TIME_SCALE = 1
+
 @onready var timer = $Timer
 
 func _on_body_entered(body):
 	print("You loose.")
+	Engine.time_scale = SLOW_TIME_SCALE
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 func _on_timer_timeout():
+	Engine.time_scale = NORMAL_TIME_SCALE
 	get_tree().reload_current_scene()
