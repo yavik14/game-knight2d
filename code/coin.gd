0a9dd1b156d1ddbc03e481d80674f845ab2ad064
The YaviK Team <theyavikteam@gmail.com>
2024-11-16 13:34:36 +0100

    Create scoring label

--- code/coin.gd
+++ code/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body):
 	print("+1 coin") 
+	game_manager.increment_score()
 	queue_free()# Replace with function body.
