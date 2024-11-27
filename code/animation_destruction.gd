8c3b649d59da9e6cf64f578afbb2381f83e650cd
Fco Javier Rodríguez Ponce <6715477+yavik14@users.noreply.github.com>
2024-11-27 20:22:10 +0100

    Feature/#15 coin animation (#14)
    * Coin tween animation

---
+++ code/animation_destruction.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@onready var coin: Area2D = $".."
+@onready var animated_sprite_2d = $"../AnimatedSprite2D"
+
+@export var distance: float = 30
+@export var duration: float = 0.5
+
+func _ready() -> void:
+	coin.body_entered.connect(_on_body_entered)
+
+func _on_body_entered(_body):
+	var tween = get_tree().create_tween().bind_node(coin).set_parallel(true)
+	# Animacion con transposicion hacia arriba de la moneda
+	tween.tween_property(coin, "position", coin.position + Vector2.UP * distance, duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	# Animacion desvanecerse el sprite
+	tween.tween_property(animated_sprite_2d, "self_modulate", Color(Color.WHITE, 0), duration)
+	# Esperar a finalizar la animacion
+	await tween.finished
+	coin.queue_free()
