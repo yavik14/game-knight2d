8dde669af36b6f5ac758c17165d4fd732a280a5a
Fco Javier Rodríguez Ponce <6715477+yavik14@users.noreply.github.com>
2024-12-01 14:23:46 +0100

    Coin selfdestruction signal (#15)
    Co-authored-by: The YaviK Team <theyavikteam@gmail.com>

--- code/coin.gd
+++ code/coin.gd
@@ -5,8 +5,19 @@ extends Area2D
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var collision_shape = $CollisionShape2D
 
+var selfdestruction: bool = true
+signal play_destruction_animation()
+
 func _on_body_entered(_body):
 	print("+1 coin") 
 	game_manager.increment_score()
 	sound.play()
 	collision_shape.call_deferred("set", "disabled", true)
+	if selfdestruction:
+		animated_sprite.visible = false
+		sound.finished.connect(_on_finished)
+	else:
+		play_destruction_animation.emit()
+
+func _on_finished():
+	queue_free()
