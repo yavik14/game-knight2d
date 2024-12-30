2d6864252e203cbd376e6775d764acb4f0dcadb2
The YaviK Team <theyavikteam@gmail.com>
2024-12-30 20:09:46 +0100

    Setup screen configuration

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # game-knight2d
-Knight 2D Godot Game
+
+Tutorial of Knight 2D Godot Game
+
+## References
+
+- [Academia de Videojuegos Tutorial Plataformas 2D con Godot](https://www.youtube.com/watch?v=okTYDaRYNVA&list=PLREdURb87ks2TtpMUiYbgzxr6WC_RsCfs)
--- code/hud.gd
+++ code/hud.gd
@@ -9,3 +9,18 @@ func _ready():
 	
 func _on_score_updated(current_score: int):
 	counter.text = str(current_score)
+
+func _input(event):
+	if event.is_action_pressed("exit"):
+		exit_game()
+	elif event.is_action_pressed("change_window_size"):
+		change_window_size()
+		
+func exit_game():
+	get_tree().quit()
+	
+func change_window_size():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
